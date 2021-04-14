e275d2714199ff824ce4fedef27130b3ab2d8d7c
binyat <binyat.sharif@gmail.com>
2021-04-14 23:45:15 +0200

    jeu

---
+++ src/main/java/com/etu/montpellier/JeuApplication.java
@@ -0,0 +1,13 @@
+package com.etu.montpellier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etu/montpellier/JeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etu.montpellier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
